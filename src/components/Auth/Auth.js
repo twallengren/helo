eb31f0fec0534336173bf9a843fb83be4ec80e78
Toren Wallengren <toren.wallengren@addepar.com>
2018-11-08 20:34:13 -0700

    some progress

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 
 class Auth extends Component {
 
+    constructor() {
+
+        super();
+
+        this.state = {
+            username: "",
+            secret: ""
+        }
+
+    }
+
+    inputBoxChange = (event, type) => {
+
+        this.setState({ [type]: event.target.value })
+
+    }
+
     render() {
 
         return (
             <div>
-                Auth
+                USERNAME: <input placeholder="username" onChange={event => this.inputBoxChange(event, "username")} />
+                PASSWORD: <input placeholder="password" onChange={event => this.inputBoxChange(event, "secret")} />
+                <button>LOGIN</button>
+                <button>REGISTER</button>
             </div>
         );
     }
