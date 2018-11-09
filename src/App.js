493ded215b6006240330af568234a1ee773efa5b
Toren Wallengren <toren.wallengren@addepar.com>
2018-11-08 18:22:54 -0700

    react setup complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Auth from './components/Auth/Auth';
+import Dashboard from './components/Dashboard/Dashboard';
+import Form from './components/Form/Form';
+import Nav from './components/Nav/Nav';
+import Post from './components/Post/Post';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Auth />
+        <Dashboard />
+        <Form />
+        <Nav />
+        <Post />
       </div>
     );
   }
